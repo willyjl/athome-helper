b4430e112733a63312ce3002d83cfeb6d9cdd72d
Willy Jansen <willyjansen@protonmail.com>
2023-01-18 10:58:36 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         athome-helper
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Athome's Helper
+// @author       Willy Jansen
+// @match        https://www.athome.co.jp/kodate/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @require      https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.min.js
+// @require      https://cdn.jsdelivr.net/npm/vue@2.7.14/dist/vue.min.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  console.log("hello");
+})();
